14f18203565374509afeadf3ac8314369ef28daa
Poplica <tu9489@gmail.com>
2020-11-05 12:02:19 -0500

    retrieved updated version of KishBot

--- Bots/KishBot.py
+++ Bots/KishBot.py
@@ -3,63 +3,87 @@ import GameState, Key, Terminal, time, Packet, Character, Field
 followMultiple = True
 followIDs = {
 	# "Main": 1,
-	# "monkaMega": 30,
-	"BlarkBlark": 23
-	,"monkaS001": 29
-	,"monkaS002": 14
-	,"monkaS003": 31
-	# ,"monkaS004": 38
+	# "Mega": 30,
+	"blark": 23
+	# ,"m01": 29
+	# ,"m02": 14
+	# ,"m03": 31
+	,"m04": 38
+	,"m05": 40
 }
 soloID = 1
 restDuration = 150
-restMap = 802000100
-
+restMap = 450001005
+# restMap = 450005431 # LP1
+# restMap = 450005430 # CLP
+# restMap = 450005510 # UP
 
 def toHex(val):
 	return hex((val + (1 << 16)) % (1 << 16)).upper()[2:].zfill(4)
 
 def castKish(x=0, y=0):
-	pos = Character.GetPos()
-	posX = toHex(pos.x + x)
-	posY = toHex(pos.y + y)
+	time.sleep(1)
+	Character.UseSkill(42111003)
+	# if x == 0 and y == 0:
+	# 	time.sleep(1)
+	# 	pos = Character.GetPos()
+	# 	x = pos.x
+	# 	y = pos.y
+	# posX = toHex(x)
+	# posY = toHex(y)
 
-	kishin = Packet.COutPacket(0x0144)
-	kishin.EncodeBuffer("** ** ** ** 0282901B 14 00 00000000 " + posX + " " + posY + " 00 00 00")
-	Packet.SendPacket(kishin)
+	# kishin = Packet.COutPacket(0x0147)
+	# kishin.EncodeBuffer("** ** ** ** 0282901B 14 00 00000000 " + posX + " " + posY + " 00 00 00")
+	# Packet.SendPacket(kishin)
 
-def waitForRush(kishDelay=.3, breakDelay=4):
+def waitForRush(expectedRushWaitDuration=6, expectedRushDuration=15):
 	startTime = time.time()
-	while not Terminal.IsRushing() and time.time() < startTime + breakDelay:
+	while not Terminal.IsRushing():
 		time.sleep(.5)
+		if time.time() > startTime + expectedRushWaitDuration:
+			Terminal.StopRush()
+			break
 	startTime = time.time()
-	while Terminal.IsRushing() and time.time() < startTime + breakDelay:
-		time.sleep(kishDelay)
+	while Terminal.IsRushing():
+		time.sleep(.3)
+		if time.time() > startTime + expectedRushDuration:
+			Terminal.StopRush()
+			# Terminal.Logout()
+			Terminal.SendLog("**Rush took too long**")
+			break
 
 
-if GameState.IsInGame() and not Terminal.IsRushing():
+if GameState.IsInGame():
+	Terminal.StopRush()
 	if followMultiple:
-		for i in followIDs.values():
-			Terminal.SetFollowID(i)
+		followList = sorted(followIDs.keys())
+		for i in followList:
+			Terminal.SetFollowID(followIDs[i])
 			waitForRush()
 
-			mapid = Field.GetID()
-			if mapid == 450005411: # UP2
-				castKish(-3000, 400)
-			elif mapid == 450007070: # LS7
-				castKish(-1500, -750)
+			mapID = Field.GetID()
+			if mapID == 450005411: # UP2
+				castKish(-3650, 92)
+			elif mapID == 450007070: # LS7
+				castKish(-1420, -705)
+			elif mapID == 450007160:
+				castKish(1050, 215)
 			else:
 				castKish()
 	else:
 		Terminal.SetFollowID(soloID)
-		waitForRush(1)
+		waitForRush()
 		castKish()
 
 	Terminal.SetFollowID(0)
 	while Field.GetID() != restMap:
 		Terminal.Rush(restMap)
-		time.sleep(.5)
+		time.sleep(2)
 
 	if followMultiple:
-		time.sleep(restDuration - len(followIDs) * 5)
-	else:
-		time.sleep(restDuration)
+		ch = Terminal.GetLocalUser(followIDs[followList[0]]).channel
+		# ch = 20
+		while GameState.GetChannel() != ch:
+			Terminal.ChangeChannel(ch)
+			time.sleep(3)
+	time.sleep(restDuration - len(followIDs) * 6 if followMultiple else restDuration)
